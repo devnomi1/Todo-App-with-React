eef539fbaa2e3459fdac9faa0c8fdae47dc5b704
Nouman Khan <noumanijaz.dev@outlook.com>
2021-11-25 14:54:27 +0500

    Change the title of App

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function Header() {
-    return (
-			<header>
-				<h1>Ed's Todo List</h1>
-			</header>
-		);
+	return (
+		<header>
+			<h1>Nomi's Todo List</h1>
+		</header>
+	);
 }
 
 export default Header;
